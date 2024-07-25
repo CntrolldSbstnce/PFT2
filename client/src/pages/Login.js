b7bab188d493104870fc07ddc1fc54dd3675c083
CntrolldSbstnce <reginaldmgriffinjr@protonmail.ch>
2024-07-25 01:19:37 -0400

    all

---
+++ client/src/pages/Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/api/users/login', formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+      <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
